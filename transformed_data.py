b75b7b0dd8315f48c70085da294c19f7a5710bca
snadikattu <48895176+snadikattu@users.noreply.github.com>
2023-05-12 16:30:07 -0500

    ETL Pipelines are added

---
+++ transformed_data.py
@@ -0,0 +1,14 @@
+# Data Transformation Logic to Clean, Filter, and Enrich Extracted Data
+
+# Import Required Libraries
+import pandas as pd
+
+# Read in Extracted Data
+extracted_data = pd.read_csv('extracted_data.csv')
+
+# Clean Data
+# Filter Data
+# Enrich Data
+
+# Write Transformed Data to CSV
+transformed_data.to_csv('transformed_data.csv', index=False)
